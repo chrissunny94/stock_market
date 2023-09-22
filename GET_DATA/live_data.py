9883e72bbea3438eca55fccf7d4a2b32a1d02800
chrissunny94 <chrissunny9gmail.com>
2023-09-22 20:25:21 +0530

    added todays logs

--- GET_DATA/live_data.py
+++ GET_DATA/live_data.py
@@ -1,8 +1,13 @@
 from jugaad_data.nse import NSELive 
+from jugaad_data.holidays import holidays
+
+print(holidays(year=2023,month=None))
+
+
 
 n = NSELive()
 import pprint
-pp = pprint.PrettyPrinter(width=41, compact=True)
-for i in range(10):
-    q = n.stock_quote("HDFCBANK")
-    pp.pprint(q['priceInfo'])
+# pp = pprint.PrettyPrinter(width=41, compact=True)
+# for i in range(10):
+#     q = n.stock_quote("HDFCBANK")
+#     pp.pprint(q['priceInfo'])
--- windows_setup/README.md
+++ windows_setup/README.md
@@ -1,3 +1,9 @@
-https://www.python.org/downloads/windows/
 
-https://gitforwindows.org/
+
+
+
+- https://www.python.org/downloads/windows/
+
+- https://gitforwindows.org/
+
+- https://gitforwindows.org/
